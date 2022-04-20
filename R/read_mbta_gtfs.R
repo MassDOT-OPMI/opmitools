8053885bfdebcf9936144eb8648096d4d94c241c
kmeakinmbta <kmeakin@mbta.com>
2022-04-20 14:21:27 -0400

    document read_mbta_gtfs()

--- R/read_mbta_gtfs.R
+++ R/read_mbta_gtfs.R
@@ -1,3 +1,17 @@
+#' Read MBTA GTFS
+#'
+#' \code{read_mbta_gtfs} reads the current GTFS from the MBTA website.
+#' Optionally, it filters to a minimal GTFS, containing only
+#' agency.txt, calendar.txt, routes.txt, shapes.txt,
+#' stop_times.txt, stops.txt, and trips.txt
+#'
+#' @param ... arguments passed on to \code{\link[tidytransit]{read_gtfs}}
+#' @param minimal A logical value, indicating whether the GTFS should contain
+#' only files indicated above
+#'
+#' @return A GTFS object
+#' @export
+#'
 read_mbta_gtfs <- function(..., minimal = TRUE) {
 
   gtfs <- tidytransit::read_gtfs("https://cdn.mbta.com/MBTA_GTFS.zip", quiet = FALSE)
