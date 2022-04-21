983064f94836e4d7d80479694fd154203f847d4e
kmeakinmbta <kmeakin@mbta.com>
2022-04-21 16:26:10 -0400

    Add rt_ridership

---
+++ R/rt_ridership.R
@@ -0,0 +1,15 @@
+#' MBTA Rapid Transit Ridership
+#'
+#' Dataset showing average fall ridership on Red, Orange, Green, and Blue Lines.
+#'
+#'
+#' @format A data frame with 36 rows and 4 columns.
+#' \describe{
+#'   \item{fall_yr}{Numeric, applicable fall rating}
+#'   \item{route_id}{Factor, Route ID of service}
+#'   \item{day_type_name}{Character, day type of service}
+#'   \item{tot_ons}{Numeric, average total boardings of service}
+#' }
+#'
+#' @source \url{https://mbta-massdot.opendata.arcgis.com/datasets/MassDOT::mbta-rail-ridership-by-time-period-season-route-line-and-stop/explore}
+"rt_ridership"
---
+++ data-raw/rt_ridership.R
@@ -0,0 +1,14 @@
+## code to prepare `rt_ridership` dataset goes here
+library(readr)
+library(dplyr)
+library(stringr)
+rt_csv <- read_csv("./data-raw/Line,_and_Stop.csv")
+
+rt_ridership <-
+  rt_csv %>%
+  group_by(fall_yr = as.numeric(str_remove(season, "Fall ")), route_id, day_type_name) %>%
+  summarize(tot_ons = sum(average_ons))
+rt_ridership <-
+  rt_ridership %>%
+  mutate(route_id = factor(route_id, levels = c("Red", "Orange", "Green", "Blue")))
+usethis::use_data(rt_ridership, overwrite = TRUE)
