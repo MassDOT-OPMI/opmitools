198ee876794ac3b55b9be299b4c38d603e7ad8ba
Karl <90868152+kmeakinmbta@users.noreply.github.com>
2023-01-30 12:07:31 -0500

    update gtfs_merge()
    add multi_route_trips

--- R/gtfs_merge.R
+++ R/gtfs_merge.R
@@ -4,6 +4,10 @@
 #' files into R, try using \code{\link{read_mbta_gtfs}} or
 #' \code{\link[tidytransit]{read_gtfs}}.
 #'
+#' This function is set up to merge the following files: `agency.txt`, `calendar.txt`,
+#'  `calendar_attributes.txt`, `multi_route_trips.txt`, `routes.txt`, `shapes.txt`, `stop_times.txt`,
+#'  `stops.txt`, `trips.txt`
+#'
 #' @param gtfs1 A GTFS file stored as a list
 #' @param gtfs2 A GTFS file stored as a list
 #'
@@ -24,14 +28,34 @@ gtfs_merge <- function(gtfs1, gtfs2) {
   gtfs$stops <- dplyr::bind_rows(gtfs1$stops, gtfs2$stops) %>% dplyr::distinct()
   gtfs$trips <- dplyr::bind_rows(gtfs1$trips, gtfs2$trips) %>% dplyr::distinct()
 
-  # if there is a calendar attributes in both files, merge that as well and then return, otherwise just return
+  # some GTFSes do not have multi_route_trips tables
+  if (!rlang::is_null(gtfs1$multi_route_trips) &
+      !rlang::is_null(gtfs2$multi_route_trips)) {
+    gtfs$multi_route_trips <-
+      bind_rows(gtfs1$multi_route_trips, gtfs2$multi_route_trips) %>% dplyr::distinct()
+  } else if (!rlang::is_null(gtfs1$multi_route_trips) &
+             rlang::is_null(gtfs2$multi_route_trips)) {
+    gtfs$multi_route_trips <- gtfs1$multi_route_trips %>% dplyr::distinct()
+  } else if (rlang::is_null(gtfs1$multi_route_trips) &
+             !rlang::is_null(gtfs2$multi_route_trips)) {
+    gtfs$multi_route_trips <- gtfs2$multi_route_trips %>% dplyr::distinct()
+  }
+
+  # if there is a calendar attributes in both files, merge that as well
   if(!rlang::is_null(gtfs1$calendar_attributes) & !rlang::is_null(gtfs2$calendar_attributes)) {
     gtfs$calendar_attributes <- dplyr::bind_rows(gtfs1$calendar_attributes, gtfs2$calendar_attributes) %>% dplyr::distinct()
-    gtfs <- gtfs %>% magrittr::extract(c("agency", "calendar", "calendar_attributes", "routes", "shapes", "stop_times", "stops", "trips"))
-    gtfs
+  }
+
+
+  if (!rlang::is_null(gtfs$calendar_attributes) & !is_null(gtfs$multi_route_trips)) {
+    gtfs["agency", "calendar", "calendar_attributes", "multi_route_trips", "routes", "shapes", "stop_times", "stops", "trips"]
+  } else if (!rlang::is_null(gtfs$calendar_attributes) & is_null(gtfs$multi_route_trips)) {
+    gtfs["agency", "calendar", "calendar_attributes", "routes", "shapes", "stop_times", "stops", "trips"]
+  } else if (rlang::is_null(gtfs$calendar_attributes) & !is_null(gtfs$multi_route_trips)) {
+    gtfs["agency", "calendar", "multi_route_trips", "routes", "shapes", "stop_times", "stops", "trips"]
   } else {
-    gtfs <- gtfs %>% magrittr::extract(c("agency", "calendar", "routes", "shapes", "stop_times", "stops", "trips"))
-    gtfs
+    gtfs["agency", "calendar", "routes", "shapes", "stop_times", "stops", "trips"]
   }
 
 }
+
